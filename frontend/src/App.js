028b63921572da71060609d7fa40460b2ae1a145
Airegasm <admin@airegasm.com>
2026-03-31 02:21:11 -0400

    Bump to v3.9.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -391,7 +391,7 @@ function App() {
 
   return (
     <div className={`app chat-layout ${isModalOpen ? 'modal-open' : ''} ${isFlowsPage ? 'flows-page' : ''}`}>
-      <span className="version-badge">v3.9.4</span>
+      <span className="version-badge">v3.9.5</span>
       {/* Top metallic frame border */}
       <div className="top-frame-border"></div>
 
